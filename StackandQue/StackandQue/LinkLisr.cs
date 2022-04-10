7450bc02a54cc6a2c0fd1cb14840ce0c015c8656
ShubhamG1234 <shubhamgole28031996@gmail.com>
2022-04-10 15:55:06 +0530

    initial commit

---
+++ StackandQue/StackandQue/LinkLisr.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks_And_Queues
+{
+    internal class LinkedListStack
+    {
+        public Node top;
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+
+
+        /// Method to push element in the stack
+
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine(value + " pushed to stack");
+        }
+
+
+        /// Method to display stack
+
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
